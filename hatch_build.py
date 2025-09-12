297ffc860e576a6f12f5beeff29f5696229144b5
Mikael Göransson <github@mgor.se>
2025-09-12 12:21:37 +0200

    (all) tuning builds
    verifying that sdist and wheels contain expected files and metadata.

--- hatch_build.py
+++ hatch_build.py
@@ -4,9 +4,11 @@ from __future__ import annotations
 
 import subprocess
 from os import environ
+from pathlib import Path
 from typing import Any
 
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+from hatchling.metadata.plugin.interface import MetadataHookInterface
 
 
 class BuildGrizzly(BuildHookInterface):
@@ -20,6 +22,16 @@ class BuildGrizzly(BuildHookInterface):
                 message = f'"{e.cmd}" got exit code {e.returncode}: {e.output}'
                 raise RuntimeError(message) from e
 
+    def is_dynamic_readme(self) -> bool:
+        return 'readme' in self.metadata.dynamic
+
+    def _finialize_dynamic_readme(self) -> None:
+        if not self.is_dynamic_readme():
+            return
+
+        readme = Path.cwd() / 'README.md'
+        readme.unlink()
+
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
         match self.metadata.name:
             case 'grizzly-loadtester-ls':
@@ -28,3 +40,29 @@ class BuildGrizzly(BuildHookInterface):
                 pass
 
         return super().initialize(version, build_data)
+
+    def finalize(self, version: str, build_data: dict[str, Any], artifact_path: str) -> None:
+        self._finialize_dynamic_readme()
+
+        super().finalize(version, build_data, artifact_path)
+
+
+class MetadataGrizzly(MetadataHookInterface):
+    def _update_readme(self, metadata: dict) -> None:
+        if 'readme' not in metadata.get('dynamic', []):
+            return
+
+        readme_source = Path.cwd().parent / 'README.md'
+        readme_dest = Path.cwd() / 'README.md'
+        readme_dest.unlink(missing_ok=True)
+
+        if not readme_source.exists():
+            message = f'{readme_source.as_posix()} does not exist'
+            raise FileNotFoundError(message)
+
+        readme_dest.write_text(readme_source.read_text())
+
+        metadata.update({'readme': 'README.md'})
+
+    def update(self, metadata: dict) -> None:
+        self._update_readme(metadata)
